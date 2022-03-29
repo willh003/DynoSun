79e8ec058a629073f17f2d7d1352e359a1331a26
wyasul <wyattsullivan02@gmail.com>
2022-03-29 00:06:47 -0400

    Update window.py

--- src/window.py
+++ src/window.py
@@ -4,8 +4,9 @@ import csv
 class Window():
 
     def __init__(self, coordinates, pointIndices, area, energyFlow=1, transfer_coefficient=1):        
-        # @param coordinates: a list of coordinates defining the location of the window
-        # ex. for a rectangular window, [[325, 412.43, 413.34], [320, 412.43, 413.34], [325, 408.43, 410.34]]
+        # @param coordinates: a list of four coordinates defining the location of the window's four corners
+        # The order of coordinates is top left, rop right, bottom left, bottom right
+        # ex. for a rectangular window, [[320, 412.43, 413.34], [325, 412.43, 413.34], [320, 408.43, 410.34], [325, 408.43, 410.34]]
 
         # @param pointIndices: a list of indices of the points in this window in the specified 
         # csv files (returned by getPointIndices())
@@ -19,8 +20,14 @@ class Window():
         self.area = area
         self.energyFlow = energyFlow
         self.transfer_coefficient = transfer_coefficient
-        
 
+    def cleanData(self, data):
+        for i in range(len(data)):
+            for j in range(len(data[0])):
+                data[i][j] = data[i][j].replace("{","")
+                data[i][j] = data[i][j].replace("}","")
+                data[i][j] = float(data[i][j])
+        
     def getPointIndices(self):
         self.pointIndices = getPoints(self.coordinates, filepath)
 
@@ -33,12 +40,17 @@ class Window():
         with open(filepath) as f:
             reader = csv.reader(f)
             data = list(reader)
+        self.cleanData(data)
+        filteredList = []
+        for i in range(len(data)):
+            if (float(data[i][0]) > self.coordinates[0][0] and float(data[i][0]) < self.coordinates[1][0] and 
+            float(data[i][1]) > self.coordinates[2][1] and float(data[i][1]) < self.coordinates[0][1]):
+                filteredList = filteredList + [data[i]]
+        return filteredList
 
-        pass 
 
     def getArea(self):
         # @return: area of window, based on window coordinates (for a rectangle, just base * height) 
-        
         pass
 
     def getWindowEnergyFlow(self):
@@ -48,8 +60,9 @@ class Window():
 
         pass
 
-window = Window([214], 20, 10)
-window.getPoints('resources/point_locations.csv')
+window = Window([[800, 250, 413.34], [900, 250, 413.34], [800, 200, 410.34], [900, 200, 410.34]]
+, 20, 10)
+print(window.getPoints('resources/point_locations.csv'))
 
 
 
