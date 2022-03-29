b98c8e5813be5d3b956c5cc0da0f59289a0a6914
willh003 <willhu003@gmail.com>
2022-03-29 18:01:11 -0400

    Finished crude window

--- src/room.py
+++ src/room.py
@@ -1,9 +1,10 @@
+import numpy as np
 
 class Room():
 
-    def __init__(self, dimensions, maxCapacity, windows, location, heatLag=0):
-        # TODO: standardize room location format. Do we use room numbers? Or another arbritray system
-        # @param dimensions: the xyz dimensions of the room (in meters)
+    def __init__(self, dimensions, maxCapacity, windows, location, energyFlow = 0, heatLag=0):
+        # TODO: standardize room location format. Do we use room numbers? Or another arbritray system (maybe Rhino coords?)
+        # @param dimensions: the xyz dimensions of the room (in meters) OR MAYBE we should use corner points (then we can locate it)
         # @param heatLag: the estimated lag of heat in the room from light (low priority right now)
 
         self.volume = getVolume(dimensions)
@@ -11,18 +12,21 @@ class Room():
         self.maxCapacity = maxCapacity
         self.windows = windows # list of window objects in this room
         self.location = location
-        self.energyFlow = 0
+        self.energyFlow = energyFlow
 
     def getVolume(self, dimensions):
-        # return x*y*z
-        pass
+        # May need to change this, based on dimensions spec
+        return np.prod(dimensions)
 
     def getSurfaceAreas(self, dimensions):
         # return list containing surface areas for walls, floors, ceilings
         # low priority (not necessary for prototype)
         pass
 
+    def setRoomEnergyFlow(self):
+        self.energyFlow = getRoomEnergyFlow()
+
     def getRoomEnergyFlow(self):
         # return heuristic value for this room
-        # sum of window energy flows divided by room volume
-        pass
+        # sum of window energy flows divided by room volume (maybe multiply by some scalar)
+        return np.sum(self.windows) / self.volume
--- src/window.py
+++ src/window.py
@@ -1,9 +1,12 @@
 import csv 
-
+import numpy as np
+import pandas as pd
+# TODO: we may want to make separate methods from loading in our csv files, since we are going to be doing it a lot
+# TODO: unit test all methods
 
 class Window():
 
-    def __init__(self, coordinates, pointIndices, area, energyFlow=1, transfer_coefficient=1):        
+    def __init__(self, coordinates, area, pointLocFile, energyFlow=0, transfer_coefficient=1):        
         # @param coordinates: a list of four coordinates defining the location of the window's four corners
         # The order of coordinates is top left, rop right, bottom left, bottom right
         # ex. for a rectangular window, [[320, 412.43, 413.34], [325, 412.43, 413.34], [320, 408.43, 410.34], [325, 408.43, 410.34]]
@@ -12,14 +15,17 @@ class Window():
         # csv files (returned by getPointIndices())
 
         # @param area: the area of the window (returned by getArea) 
+
         # @param transfer_coefficient: value 0-1, based on transparency of window (1 is completely transparent)
         # sort of arbritray for now
+
+        # @param pointLocFile: the path to the file containing the locations in 3 space of the points
         
         self.coordinates = coordinates
-        self.pointIndices = pointIndices
-        self.area = area
-        self.energyFlow = energyFlow
+        self.area = self.getArea(coordinates)
         self.transfer_coefficient = transfer_coefficient
+        self.setPointIndices(pointLocFile)
+        self.energyFlow = energyFlow # initialize to 0 (user can override)
 
     def cleanData(self, data):
         for i in range(len(data)):
@@ -28,8 +34,8 @@ class Window():
                 data[i][j] = data[i][j].replace("}","")
                 data[i][j] = float(data[i][j])
         
-    def getPointIndices(self):
-        self.pointIndices = getPoints(self.coordinates, filepath)
+    def setPointIndices(self, filepath):
+        self.pointIndices = self.getPoints(filepath)
 
     def getPoints(self, filepath):        
         # @param filepath: path to open the csv containing point locations
@@ -42,28 +48,49 @@ class Window():
             data = list(reader)
         self.cleanData(data)
         filteredList = []
+        indexList = []
         for i in range(len(data)):
             if (float(data[i][0]) > self.coordinates[0][0] and float(data[i][0]) < self.coordinates[1][0] and 
             float(data[i][1]) > self.coordinates[2][1] and float(data[i][1]) < self.coordinates[0][1]):
-                filteredList = filteredList + [data[i]]
-        return filteredList
+                filteredList.append([data[i]])
+                indexList.append(i)
+        return indexList
 
-
-    def getArea(self):
+    def getArea(self, coordinates):
         # @return: area of window, based on window coordinates (for a rectangle, just base * height) 
-        pass
+        # |u x v| -> area of parallelogram in 3 space
+        # TODO: test this method
+        v1 = [coordinates[0][0] - coordinates[1][0], coordinates[0][1] - coordinates[1][1], coordinates[0][2] - coordinates[1][2]]
+        v2 = [coordinates[0][0] - coordinates[2][0], coordinates[0][1] - coordinates[2][1], coordinates[0][2] - coordinates[2][2]]
+        
+        cross = np.cross(v1, v2)
+        return np.sqrt(cross.dot(cross))
+        
 
-    def getWindowEnergyFlow(self):
+    def setEnergyFlow(self, filepath):
+        self.energyFlow = self.getEnergyFlow(filepath)
+
+    def getEnergyFlow(self, filepath):
+        # @param filePath: the path to the file containing the energy of the points (enumerated by index)
         # @return: comparison metric for energy flow through this window
-        # sum(energy at points in pointIndices) * window area * transfer_coefficient / # of points 
-        # higher window area, higher point energy, and higher coefficient return a larger energyflow metric 
-
-        pass
-
-window = Window([[800, 250, 413.34], [900, 250, 413.34], [800, 200, 410.34], [900, 200, 410.34]]
-, 20, 10)
-print(window.getPoints('resources/point_locations.csv'))
-
-
+        # average(energy at points in pointIndices) * window area * transfer_coefficient 
+        # higher point energy, higher window area, and higher coefficient return a larger energyflow metric 
+        
+        with open(filepath) as f:
+            reader = pd.read_csv(f)
+           
+            data = reader.values.tolist()
+        
+        energies = []
+        for i in self.pointIndices:
+            energies.append(data[i][0])
+        
+        return np.average(energies) * self.area * self.transfer_coefficient 
+        
+        
+window = Window(coordinates=[[800, 250, 413.34], [900, 250, 413.34], [800, 200, 410.34], [900, 200, 410.34]]
+, area=20, pointLocFile='resources/point_locations.csv')
+window.setEnergyFlow('resources/energy_at_points.csv')
+print(window.energyFlow)
 
         
