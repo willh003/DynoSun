23341d82d79069efe33e0055d2c09d1f0a1d00b6
willh003 <willhu003@gmail.com>
2022-04-24 21:12:39 -0400

    Update window.py

--- src/window.py
+++ src/window.py
@@ -37,7 +37,40 @@ class Window():
         self.cleanData(window)
         return window
 
-    def getPoints(self, window, pointCoords):
+    def getPoints4Corners(self, window, pointCoords):
+        # @param indexList: a list of indices of the points in this window in the specified csv files
+        # https://stackoverflow.com/questions/21037241/how-to-determine-a-point-is-inside-or-outside-a-cube#:~:text=Construct%20the%20direction%20vector%20from,is%20outside%20of%20the%20cube
+        
+        # with open(pointCoords) as f:
+        #     reader = csv.reader(f)
+        #     points = list(reader)
+        # self.cleanData(points)
+
+        # absolute values so the coordinate system is never negative (want +x, +y, +z)
+        # this might be a problem - may need to manually find the larger points and use those as the references
+        xvect = list(map(abs, np.subtract(window[1], window[0])))
+        yvect = list(map(abs, np.subtract(window[2], window[0])))
+        zvect = np.cross(xvect, yvect)
+
+        xlocal = xvect/np.linalg.norm(xvect)
+        ylocal = yvect/np.linalg.norm(yvect)
+        zlocal = zvect/np.linalg.norm(zvect)
+        
+
+        transMatrix = np.array([xlocal, ylocal, zlocal]).T # transformation matrix with new coordinate axes as columns
+        
+        filteredList = []
+        indexList = []
+        for i in range(len(window)):
+            point = window[i]
+            transPoint = np.matmul(transMatrix, np.array([point]).T) # transform the point to new axes
+            if self.isInAlignedBox(point, window, self.allowedError): # TODO: construct the point first!!
+                filteredList.append(point) # not used atm, but contains a list of point locations
+                indexList.append(i) # contains a list of point indices
+
+        return indexList
+
+    def getPoints8Corners(self, window, pointCoords):
         # @param indexList: a list of indices of the points in this window in the specified csv files
         # https://stackoverflow.com/questions/21037241/how-to-determine-a-point-is-inside-or-outside-a-cube#:~:text=Construct%20the%20direction%20vector%20from,is%20outside%20of%20the%20cube
         
@@ -95,48 +128,6 @@ class Window():
         z = point[2]
 
         return mins[0] <= x and maxs[0] >= x and mins[1] <= y and maxs[1] >= y and mins[2] <= z and maxs[2] >= z
-
-
-    def getPointsOld(self, coordinates, filepath):    
-        # DEPRECATED    
-        # @param filepath: path to open the csv containing point locations
-        # @return: the points from the simulation contained within the window (format: index referring to the points in the csv file)
-        # TODO: where do we enter filepath? Who calls it?
-        # TODO: how do we clean the data (format is list of '{}' at the moment)
-
-        with open(filepath) as f:
-            reader = csv.reader(f)
-            data = list(reader)
-        self.cleanData(data)
-        
-        filteredList = []
-        indexList = []
-
-        v1 = [coordinates[0][0] - coordinates[1][0], coordinates[0][1] - coordinates[1][1], coordinates[0][2] - coordinates[1][2]]
-        v2 = [coordinates[0][0] - coordinates[2][0], coordinates[0][1] - coordinates[2][1], coordinates[0][2] - coordinates[2][2]]
-        
-        normVect = np.cross(v1, v2) # vector perpendicular to plane of window
-        planePoint = coordinates[0] # point on plane (given by coordinates at 0, in this case)
-        d = -np.dot(normVect, planePoint) # ax + by + cz = d plane equation
-
-        for i in range(len(data)):
-            poi = data[i]
-            normDist = abs(np.dot(normVect, poi) + d) / (np.linalg.norm(normVect))
-
-            pointVect = np.subtract(poi, planePoint)
-
-            v1_norm = np.linalg.norm(v1)
-            proj = (np.dot(pointVect, v1) / (v1_norm ** 2))*v1 # proj of pointvect along v1
-
-            # if normal distance to plane is less than offset and 
-            # magnitude of projection along vector is less than vector magnitude
-            if normDist <= offset and np.linalg.norm(proj) <= v1_norm: 
-                filteredList.append([data[i]])
-                indexList.append(i)
-
-
-
-        return indexList
 
     def getArea(self, coordinates):
         # @return: area of window, based on window coordinates (for a rectangle, just base * height) 
